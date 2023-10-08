cbfe6ec0ef80c6a7f19cc0b6b6da3872d1e9b6dd
Sebastiaan Stok <s.stok@rollerscapes.net>
2023-10-08 20:52:47 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// See https://www.park-manager.com/hubkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'sstok/hubkit',
+    'schema_version' => 2,
+];
